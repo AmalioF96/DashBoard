088a7edf640b48e0854e31079bee86536c5510dd
Amalio <amaliocabeza.16@gmail.com>
2020-06-25 11:49:00 +0200

    Mejoras Codigo
    Adición de gráficas y eliminación de códigos de prueba

--- vista/main.py
+++ vista/main.py
@@ -9,7 +9,10 @@ from vista.view import view
 
 if __name__ == '__main__':
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    
     miVista=view(app)
     miVista.mostrarTodo()
+    
     app.run_server(debug=True)
